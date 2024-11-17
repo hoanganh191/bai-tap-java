0cf2178cb885615f1b89ba8a1d6aa3d51f8001be
hoanganh191 <kbhoanganh05@gmail.com>
2024-11-17 23:15:53 +0700

    Cap nhat bai can bo san xuat btvn

---
+++ Can_Bo_San_Xuat/chayBaiTap.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Can_Bo_San_Xuat;
+
+/**
+ *
+ * @author hoanganh
+ */
+public class chayBaiTap {
+    public static void main(String[] args) {
+        //Ae khai bao cac thuc the tu cac lop va chay code, vi du ky su
+        KySu a = new KySu();
+        a.nhap();
+        System.out.println(""); //Tao khoang trang
+        System.out.println("Thong tin cua ky su vua nhap la");
+        a.hienThi();
+    }
+}
